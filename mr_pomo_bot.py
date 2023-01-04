c6c88f16e12f131ff9f482334458e492a863f196
nij-patel <nijpatel28@gmail.com>
2023-01-03 22:18:12 -0500

    added couple more methods and cleaned up timer

--- mr_pomo_bot.py
+++ mr_pomo_bot.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import discord
 import asyncio
+from timer import Timer
 from discord.ext import commands
 
 load_dotenv()
@@ -12,6 +13,8 @@ intents = discord.Intents.default()
 
 # client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='!')
+
+timer = Timer()
 
 # @client.event
 
@@ -27,10 +30,17 @@ async def start_timer(ctx):
     start_work_em = discord.Embed(
         title="Time to start working!", color=0x33c633)
     await ctx.send(embed=start_work_em)
-    await asyncio.sleep(5)
+
+    timer.start()
+    while timer.get_status():
+        await asyncio.sleep(1)  # 25 * 60
+        timer.tick()
+        if timer.get_ticks() >= 10:
+            timer.stop()
+
     start_play_em = discord.Embed(
         title="Time to start your break!", color=0x33c633)
-    await ctx.send(emded=start_play_em)
+    await ctx.send(embed=start_play_em)
 
 
 @bot.command(name="stop", help="Stop a Pomodoro timer")
@@ -38,6 +48,25 @@ async def stop_timer(ctx):
     stop_timer_em = discord.Embed(
         title="Timer has been stopped!", color=0xc63333)
     await ctx.send(embed=stop_timer_em)
+    timer.stop()
+
+
+@bot.command(name="time", help="Show current time")
+async def show_time(ctx):
+    await ctx.send(f"Current timer status is :  {timer.get_status()}")
+    await ctx.send(f"Current time is :  {timer.get_ticks()}")
+
+
+@bot.command(name="help2", help="Show help text")
+async def show_help(ctx):
+    help_commands = dict()
+    for command in bot.commands:
+        help_commands[command.name] = command.help
+    description = "Bot commands are: {}".format(help_commands)
+    show_help_em = discord.Embed(
+        title="This is Mr Pomo Dorio, a friendly pomodorio bot.", description=description, color=0xc63333)
+    await ctx.send(embed=show_help_em)
+    timer.stop()
 
 
 #    if message.author == client.user:
